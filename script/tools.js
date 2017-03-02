f0d8928a2919c906acbe6ca32f1787a32efae97d
Chris <flyernaruto@gmail.com>
2017-03-02 09:22:54 +0800

    init files

---
+++ script/tools.js
@@ -0,0 +1,74 @@
+﻿window.onload=function(){preprocess();}
+document.onwheel = function() {clearInterval(scrollFun);}
+window.onscroll=function(){changeNav();}
+
+function navMark(){
+	document.getElementById("navscroll").innerHTML=document.getElementById("navori").innerHTML;
+	if(document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+		document.getElementById("navori").style.display="none";
+		document.getElementById("navscroll").style.display="block";
+	}else{
+		document.getElementById("navori").style.display="block";
+		document.getElementById("navscroll").style.display="none";
+	}
+	
+}
+function preprocess(){
+	navMark();
+	var headnews=document.getElementById("headnews");
+	var other=document.getElementById("othernews");
+	headnews.children[0].children[0].children[0].children[0].innerHTML='<i class="fa fa-spinner fa-pulse fa-1x fa-fw margin-bottom"></i> Loading';
+	for(i=0;i<4;i++){
+		other.children[0].children[Math.floor(i/2)].children[i%2].children[0].innerHTML='<i class="fa fa-spinner fa-pulse fa-1x fa-fw margin-bottom"></i> Loading';
+	}
+	
+}
+
+function changeNav(){
+	var html = document.documentElement;
+	if(document.body.scrollTop!=0 || html.scrollTop!=0){
+		document.getElementById("navori").style.display="none";
+		document.getElementById("navscroll").style.display="block";
+	}else{
+		document.getElementById("navori").style.display="block";
+		document.getElementById("navscroll").style.display="none";
+	}
+}
+var scrollFun;
+function moveTo(pos){
+	var sy = document.documentElement.scrollTop;
+	if(sy<document.body.scrollTop)sy=document.body.scrollTop;
+	var speed=1;
+	var delta=(pos-sy);
+	if(Math.abs(delta)<speed || pos<0){
+		clearInterval(scrollFun);
+		return;
+	}
+	if(sy<pos){
+		if(pos-sy<speed){
+			document.body.scrollTop=pos;
+			document.documentElement.scrollTop=pos;
+		}else{
+			
+			document.body.scrollTop+=delta/20+speed;
+			document.documentElement.scrollTop+=delta/20+speed;
+		}
+	}else if(sy>pos){
+		if(sy-pos<speed){
+			document.body.scrollTop=pos;
+			document.documentElement.scrollTop=pos;
+		}else{
+			document.body.scrollTop+=delta/20-speed;
+			document.documentElement.scrollTop+=delta/20-speed;
+		}
+	}
+	
+}
+
+function scrollGoTo(pos){
+	if(pos>document.body.scrollHeight-window.innerHeight){
+		pos=document.body.scrollHeight-window.innerHeight;
+	}
+	clearInterval(scrollFun);
+	scrollFun=setInterval(function(){moveTo(pos)},10);
+}
